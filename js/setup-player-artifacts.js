f7c1a7205f4facafa2d90f8df7b5330b1a928f76
fuckhtml <jordenblacksnk@gmail.com>
2020-11-01 16:29:59 +0300

    Implements artifacts drag-and-drop

--- js/setup-player-artifacts.js
+++ js/setup-player-artifacts.js
@@ -9,34 +9,54 @@
       if (event.target.classList.contains('artifact-item')) {
 
         const documentOnMouseMove = () => {
-          artifactElement.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+          artifactElement.style.transform = `translate(${event.clientX + 32}px, ${event.clientY + 30}px)`;
           Array.from(setupArifactsCellElementList).map(cellElement => cellElement.style.border = '');
-          if (event.target.parentElement.className === 'setup-artifacts' && event.target.className === 'setup-artifacts-cell') {
-            event.target.style.border = '5px solid white';
+          
+          if (
+            event.clientX >= setupArtifactsElement.getBoundingClientRect().x &&
+            event.clientX <= setupArtifactsElement.getBoundingClientRect().x + setupArtifactsElement.getBoundingClientRect().width &&
+            event.clientY >= setupArtifactsElement.getBoundingClientRect().y &&
+            event.clientY <= setupArtifactsElement.getBoundingClientRect().y + setupArtifactsElement.getBoundingClientRect().height
+          ) {
+            mouseInsideSetupArtifactCellIndex = 
+              Math.floor((event.clientY - setupArtifactsElement.getBoundingClientRect().y) /
+              setupArifactsCellElementList[0].getBoundingClientRect().width) * 4 +
+              Math.floor((event.clientX - setupArtifactsElement.getBoundingClientRect().x) /
+              setupArifactsCellElementList[0].getBoundingClientRect().width);
+          } else {
+            mouseInsideSetupArtifactCellIndex = null;
+          }
+
+          if (mouseInsideSetupArtifactCellIndex !== null) {
+            setupArifactsCellElementList[mouseInsideSetupArtifactCellIndex].style.border = '5px solid white';
           }
         }
 
         const documentOnMouseUp = () => {
-          document.body.style = '';
-          if (event.target.parentElement.className === 'setup-artifacts' && event.target.className === 'setup-artifacts-cell') {
-            Array.from(setupArifactsCellElementList).map(cellElement => cellElement.style.border = '');
-            artifactElement.classList.remove('artifact-item');
+          if (mouseInsideSetupArtifactCellIndex !== null) {
+            setupArifactsCellElementList[mouseInsideSetupArtifactCellIndex].innerHTML = '';
+            setupArifactsCellElementList[mouseInsideSetupArtifactCellIndex].appendChild(artifactElement);              
             artifactElement.classList.remove('artifact-item-on-drag');
             artifactElement.style = '';
-            event.target.appendChild(artifactElement);
           } else {
             document.body.removeChild(artifactElement);
           }
+          document.body.style = '';
+
+          Array.from(setupArifactsCellElementList).map(cellElement => cellElement.style.border = '');
+
           document.removeEventListener('mousemove', documentOnMouseMove);
           document.removeEventListener('mouseup', documentOnMouseUp);
         }
 
         let artifactElement = event.target.cloneNode(false);
         artifactElement.classList.add('artifact-item-on-drag');
-        artifactElement.style.transform = `translate(${event.clientX}px, ${event.clientY})`;
+        artifactElement.style.transform = `translate(${event.clientX + 32}px, ${event.clientY + 30}px)`;
 
-        document.body.style = 'position: relative; cursor: grabbing';
+        document.body.style = 'position: relative;';
         document.body.appendChild(artifactElement);
+
+        let mouseInsideSetupArtifactCellIndex = null;
 
         document.addEventListener('mousemove', documentOnMouseMove, false)
         document.addEventListener('mouseup', documentOnMouseUp, false)
@@ -45,7 +65,8 @@
 
     const setupElement = document.querySelector('.setup');
     const setupArtifactsShopElement = setupElement.querySelector('.setup-artifacts-shop');
-    const setupArifactsCellElementList = setupElement.querySelectorAll('.setup-artifacts-cell');
+    const setupArtifactsElement = setupElement.querySelector('.setup-artifacts');
+    const setupArifactsCellElementList = setupArtifactsElement.querySelectorAll('.setup-artifacts-cell');
     setupArtifactsShopElement.addEventListener('mousedown', artifactsShopOnMouseDown, false);
   }
 
